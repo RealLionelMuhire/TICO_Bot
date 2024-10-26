2ad9a77fbf0afb561beecde387ee52e399e4b921
RealLionelMuhire <muhirelionel@gmail.com>
2024-10-26 17:58:33 +0200

    creating react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Homepage from './routes/homepage/Homepage.jsx';
+import Dashboard from './routes/dashboard/Dashboard.jsx';
+import Chatpage from './routes/chatpage/Chatpage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />
+  },
+  {
+    path: "dashboard",
+    // element: <Dashboard />,
+    children: [
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/dashboard/chats/:id", element: <Chatpage /> }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
